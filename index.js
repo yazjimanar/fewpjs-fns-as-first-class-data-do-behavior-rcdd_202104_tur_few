8ef06308421ab4e58a8ca2d88eed71132eea1ce1
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-13 15:58:59 +0000

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeStr){
+  let nowTime = timeStr.split(':');
+  let nowHour = parseInt(nowTime[0]);
+  if (nowHour<12){
+    return "Good Morning";
+  } else if (nowHour>17){
+    return "Good Evening";
+  }
+  return "Good Afternoon";
+}
+/* Write your implementation of displayMessage() */
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+
+}
